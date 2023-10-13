1d5834b50f17eb841d4f5f3fbfac5efa656a632a
connono <q1063496374@qq.com>
2023-10-13 14:02:39 +0800

    fix(pretty): pretty code

--- src/pages/UserList/index.tsx
+++ src/pages/UserList/index.tsx
@@ -1,21 +1,20 @@
 import { SERVER_HOST } from '@/constants';
-import { useRef, useState } from 'react';
-import { 
-  PageContainer, 
-  ProColumns, 
-  ProTable, 
-  ModalForm, 
-  ProForm,
-  ProFormText,
+import type { ProFormInstance } from '@ant-design/pro-components';
+import {
+  ActionType,
+  ModalForm,
+  PageContainer,
+  ProColumns,
   ProFormDigit,
   ProFormSelect,
-  ActionType,
+  ProFormText,
+  ProTable,
 } from '@ant-design/pro-components';
 import { Access, useAccess, useRequest } from '@umijs/max';
-import { Affix, Button, message } from 'antd';
+import { Button, message } from 'antd';
 import axios from 'axios';
-import type { ProFormInstance } from '@ant-design/pro-components';
 import _ from 'lodash';
+import { useRef, useState } from 'react';
 
 enum MODE {
   CREATE,
@@ -23,44 +22,53 @@ enum MODE {
 }
 
 type UserInfo = {
-  id: number,
-  name: string,
-  phone_number: number,
-  department: string,
+  id: number;
+  name: string;
+  phone_number: number;
+  department: string;
 };
 
-export const departmentData = [{
-  value: 'bf013',
-  label: '外一科'
-},{
-  value: 'bf012',
-  label: '外二科'
-},{
-  value: 'bf010',
-  label: '骨二科'
-},{
-  value: 'bf006',
-  label: 'ICU病区'
-},]
-
+export const departmentData = [
+  {
+    value: 'bf013',
+    label: '外一科',
+  },
+  {
+    value: 'bf012',
+    label: '外二科',
+  },
+  {
+    value: 'bf010',
+    label: '骨二科',
+  },
+  {
+    value: 'bf006',
+    label: 'ICU病区',
+  },
+];
 
 const getUserList = async () => {
   return await axios.get(`${SERVER_HOST}/users/index`);
-}
+};
 
 const getAllRoles = async () => {
   return await axios.get(`${SERVER_HOST}/allRoles`);
-}
+};
 
 const getAllDepartments = async () => {
   return await axios.get(`${SERVER_HOST}/department/index`);
-}
+};
 
 const deleteUser = async (id: number) => {
   return await axios.delete(`${SERVER_HOST}/users/${id}`);
-}
+};
 
-const createUser = async (name: string, phone_number: number, department: string, roles: string[]) => {
+const createUser = async (
+  name: string,
+  phone_number: number,
+  department: string,
+  roles: string[],
+) => {
   return await axios({
     method: 'POST',
     data: {
@@ -69,11 +77,16 @@ const createUser = async (name: string, phone_number: number, department: string
       phone_number,
       roles,
     },
-    url: `${SERVER_HOST}/users`
+    url: `${SERVER_HOST}/users`,
   });
-}
+};
 
-const updateUser = async (id: number, phone_number: number, department: string, roles: string[]) => {
+const updateUser = async (
+  id: number,
+  phone_number: number,
+  department: string,
+  roles: string[],
+) => {
   return await axios({
     method: 'PATCH',
     data: {
@@ -83,7 +96,7 @@ const updateUser = async (id: number, phone_number: number, department: string, 
     },
     url: `${SERVER_HOST}/users/${id}`,
   });
-}
+};
 
 const initialPassword = async (id: number) => {
   return await axios({
@@ -92,8 +105,8 @@ const initialPassword = async (id: number) => {
       password: '123456',
     },
     url: `${SERVER_HOST}/users/reset/${id}`,
-  })
-}
+  });
+};
 
 const UserListPage: React.FC = () => {
   const access = useAccess();
@@ -103,93 +116,91 @@ const UserListPage: React.FC = () => {
   const [mode, setMode] = useState<MODE>(MODE.CREATE);
   const [selectedId, setSelectedId] = useState<number>(0);
 
-  const { run : runGetUserList } = useRequest(getUserList, {
+  const { run: runGetUserList } = useRequest(getUserList, {
     manual: true,
-    onSuccess: (result, params) => {
+    onSuccess: (result: any) => {
       console.log('result:', result);
     },
-    onError: (error) => {
+    onError: (error: any) => {
       message.error(error.message);
-    }
-  });
-  const { run : runGetAllRoles } = useRequest(getAllRoles, {
-    manual: true,
-    onSuccess: (result, params) => {
     },
-    onError: (error) => {
-      message.error(error.message);
-    }
   });
-  const { run : runGetAllDepartments } = useRequest(getAllDepartments, {
+  const { run: runGetAllRoles } = useRequest(getAllRoles, {
     manual: true,
-    onSuccess: (result, params) => {
+    onSuccess: () => {},
+    onError: (error: any) => {
+      message.error(error.message);
     },
-    onError: (error) => {
-      message.error(error.message);
-    }
   });
-  const { run : runCreateUser } = useRequest(createUser, {
+  const { run: runGetAllDepartments } = useRequest(getAllDepartments, {
     manual: true,
-    onSuccess: (result, params) => {
+    onSuccess: () => {},
+    onError: (error: any) => {
+      message.error(error.message);
+    },
+  });
+  const { run: runCreateUser } = useRequest(createUser, {
+    manual: true,
+    onSuccess: () => {
       message.success('提交成功');
       setModalVisible(false);
     },
-    onError: (error) => {
+    onError: (error: any) => {
       message.error(error.message);
-    }
+    },
   });
-  const { run : runUpdateUser } = useRequest(updateUser, {
+  const { run: runUpdateUser } = useRequest(updateUser, {
     manual: true,
-    onSuccess: (result, params) => {
+    onSuccess: () => {
       message.success('提交成功');
       setModalVisible(false);
     },
-    onError: (error) => {
+    onError: (error: any) => {
       message.error(error.message);
-    }
+    },
   });
 
-  const { run : runDeleteUser } = useRequest(deleteUser, {
+  const { run: runDeleteUser } = useRequest(deleteUser, {
     manual: true,
-    onSuccess: (result, params) => {
+    onSuccess: () => {
       message.success('删除成功');
     },
-    onError: (error) => {
+    onError: (error: any) => {
       message.error(error.message);
-    }
+    },
   });
-  
-  const { run : runInitialPassword } = useRequest(initialPassword, {
+
+  const { run: runInitialPassword } = useRequest(initialPassword, {
     manual: true,
-    onSuccess: (result, params) => {
+    onSuccess: () => {
       message.success('重置密码成功');
     },
-    onError: (error) => {
+    onError: (error: any) => {
       message.error(error.message);
-    }
+    },
   });
-  
+
   const roles = async () => {
     const rolesData = await runGetAllRoles();
-    const data =  _.map(rolesData, (value: any, index: any) => {
+    const data = _.map(rolesData, (value: any) => {
       return {
         value: value,
         label: value,
-      }
-    })
+      };
+    });
     return data;
-  }
+  };
 
   const departments = async () => {
-    const {data : departmentsData} = await runGetAllDepartments();
-    const data =  _.map(departmentsData, (value: any, index: any) => {
+    const { data: departmentsData } = await runGetAllDepartments();
+    const data = _.map(departmentsData, (value: any) => {
       return {
         value: value.name,
         label: value.label,
-      }
-    })
+      };
+    });
     return data;
-  }
+  };
 
   const columns: ProColumns<UserInfo>[] = [
     {
@@ -222,9 +233,9 @@ const UserListPage: React.FC = () => {
               setSelectedId(record.id);
               setMode(MODE.UPDATE);
               setModalVisible(true);
-              setTimeout(()=>{
-                formRef.current?.setFieldsValue(record)
-              },1000)
+              setTimeout(() => {
+                formRef.current?.setFieldsValue(record);
+              }, 1000);
             }
           }}
         >
@@ -243,7 +254,7 @@ const UserListPage: React.FC = () => {
         >
           密码重置
         </a>,
-        <a 
+        <a
           key="delete"
           onClick={async () => {
             if (!access.canUpdateUser) {
@@ -256,9 +267,9 @@ const UserListPage: React.FC = () => {
         >
           删除
         </a>,
-      ]
-    }
-  ]
+      ],
+    },
+  ];
 
   return (
     <PageContainer
@@ -286,7 +297,7 @@ const UserListPage: React.FC = () => {
         }}
         dateFormatter="string"
         toolBarRender={(action) => [
-          <Access accessible={access.canCreateUser}>
+          <Access key="can_create_user" accessible={access.canCreateUser}>
             <Button
               key="button"
               onClick={() => {
@@ -298,15 +309,14 @@ const UserListPage: React.FC = () => {
             >
               新建
             </Button>
-          </Access>
-          ,
+          </Access>,
         ]}
       />
       <ModalForm<{
         name: string;
         company: string;
       }>
-        title={mode === MODE.CREATE ? "新建用户" : "更新用户"}
+        title={mode === MODE.CREATE ? '新建用户' : '更新用户'}
         formRef={formRef}
         modalProps={{
           destroyOnClose: true,
@@ -315,42 +325,53 @@ const UserListPage: React.FC = () => {
         open={modalVisible}
         submitTimeout={2000}
         onFinish={async (values: any) => {
-          mode === MODE.CREATE 
-            ? await runCreateUser(values.name, values.phone_number, values.department, values.roles)
-            : await runUpdateUser(selectedId ,values.phone_number, values.department, values.roles);
+          if (mode === MODE.CREATE) {
+            await runCreateUser(
+              values.name,
+              values.phone_number,
+              values.department,
+              values.roles,
+            );
+          } else {
+            await runUpdateUser(
+              selectedId,
+              values.phone_number,
+              values.department,
+              values.roles,
+            );
+          }
           actionRef.current?.reload();
         }}
       >
+        <ProFormText
+          width="md"
+          name="name"
+          label="用户名称"
+          placeholder="请输入用户名称"
+          rules={[{ required: true }]}
+        />
 
-          <ProFormText
-            width="md"
-            name="name"
-            label="用户名称"
-            placeholder="请输入名称"
-            rules={[{ required: true }]}
-          />
-          
-          <ProFormDigit
-            width="md"
-            name="phone_number"
-            label="手机号码"
-            placeholder="请输入手机号码"
-            rules={[{ required: true }]}
-          />
-          <ProFormSelect
-              label="所属科室"
-              name="department"
-              request={departments}
-              rules={[{ required: true }]}
-            />
-          <ProFormSelect
-            request = {roles}
-            mode="multiple"
-            allowClear
-            name="roles"
-            label="权限设置"
-            rules={[{ required: true }]}
-          />
+        <ProFormDigit
+          width="md"
+          name="phone_number"
+          label="手机号码"
+          placeholder="请输入手机号码"
+          rules={[{ required: true }]}
+        />
+        <ProFormSelect
+          label="所属科室"
+          name="department"
+          request={departments}
+          rules={[{ required: true }]}
+        />
+        <ProFormSelect
+          request={roles}
+          mode="multiple"
+          allowClear
+          name="roles"
+          label="权限设置"
+          rules={[{ required: true }]}
+        />
       </ModalForm>
     </PageContainer>
   );
