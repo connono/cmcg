5c128ad74e9577cf80503dbc35cbbb6b634fd960
connono <q1063496374@qq.com>
2023-12-28 10:16:02 +0800

    fix(advance&menu): 增加审核，修复状态刷新不及时，增加多层目录

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -81,7 +81,7 @@ const LoginPage: React.FC = () => {
           </Row>
         </div>
       </div>
-      <div className="version">版本：1.3.4</div>
+      <div className="version">版本：1.4.2</div>
     </PageContainer>
   );
 };
